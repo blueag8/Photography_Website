32233111e4a0db8d3ed22231fc05bc4f7f60a03a
blueag8 <ns.wickham08@outlook.com>
2019-11-12 20:25:42 +0000

    backends.py added

---
+++ authentication/templates/backends.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+
+
+class EmailAuth:
+    """Authenticate a user by email """
+
+    def authenticate(self, username=username, password=None):
+
+        try:
+           user = User.objects.get(email=username)
+
+           if user.check_password(password):
+               return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+
+        try:
+            user = User.objects.get(pk=user_id)
+
+            if user.is_active:
+              return user  
+            return None
+        except User.DoesNotExist:
+            return None    
+    
+
+
