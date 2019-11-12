06ed8cfcecb450b754c15a879aea149dc3911c68
blueag8 <ns.wickham08@outlook.com>
2019-11-12 21:11:45 +0000

    added Post app for userInput forms

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField()
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+
--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'django_forms_bootstrap',
+    'authentication',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -119,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
@@ -129,3 +137,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 #EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 #EMAIL_HOST_PASSWORD =os.environ.get("EMAIL_PASSWORD")
 #EMAIL_HOST_PORT = 587
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'authentication.backends.EmailAuth'
+]
