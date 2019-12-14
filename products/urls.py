d3b93f65281d8dd8b5ab9f6bd3a169b1393eb789
blueag8 <ns.wickham08@outlook.com>
2019-12-14 23:19:54 +0000

    links to individual products added

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from products.views import all_products, portfolio
+from products.views import all_products, portfolio, product,image
 
 urlpatterns = [
     url(r'^all_products/', all_products, name='products'),
     url(r'^portfolio/', portfolio, name='portfolio'),
-
-]
+    url(r'^product/(?P<id>\d+)', product, name='product'),
+    url(r'^image/(?P<id>\d+)', image, name='image'),
+    ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .models import Product
 # Create your views here.
 
-def product(request):
-    product = Product
-    return render(request, "product.html", {"product.id":product.id} )
+def product(request,id):  
+    products = Product.objects.all()
+    product = Product.objects.get(pk=id)
+    print (product.name)
+    return render(request, "product.html", {"id":id,"product":product})
 
 def all_products(request):
     products = Product.objects.all()
-    return render(request, "products.html", {"products":products})
+    return render(request, "products.html", {"products":products, "product":product})
 
 def portfolio(request):
     products = Product.objects.all()
     return render(request,"portfolio.html", {"products":products})
+
+def image(request,id):  
+    products = Product.objects.all()
+    product = Product.objects.get(pk=id)
+    return render(request,"image.html", {"id":id,"product":product, "products":products})
+
+
 
