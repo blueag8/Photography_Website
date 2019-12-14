aa604d07d9b79cd9c5385b6de43c4b631d791c2e
blueag8 <ns.wickham08@outlook.com>
2019-12-14 02:38:54 +0000

    requirements.txt fixed

--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://henkdeweerdphotography.herokuapp.com/']
 
 # Application definition
 
