7cf48a07005b8c8b6254d952e05afe3db6de2146
blueag8 <ns.wickham08@outlook.com>
2019-12-14 01:58:58 +0000

    reset head

--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://git.heroku.com/henkdeweerdphotography.git']
 
 # Application definition
 
