a474279b24c2e6dc4952df13924333c4ef15d9df
blueag8 <ns.wickham08@outlook.com>
2019-12-31 21:36:27 +0000

    removed env

--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
-import env 
 import cloudinary
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
