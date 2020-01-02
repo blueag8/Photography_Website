e96b6e9b6325d0194e8f1e20fec5c9bbac10ac3e
blueag8 <41134758+blueag8@users.noreply.github.com>
2020-01-02 23:15:33 +0000

    Update settings.py

--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import os
 import django_heroku
-impotr dj_database_url
+import dj_database_url
 import cloudinary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
