c13a3861495e083a1a1b3f91cacfd0965bf352af
blueag8 <ns.wickham08@outlook.com>
2020-01-04 20:57:09 +0000

    attempt to loaddata into postgesql

--- toolbox_app/settings.py
+++ toolbox_app/settings.py
@@ -13,6 +13,7 @@ import os
 import django_heroku
 import dj_database_url
 import cloudinary
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,12 +88,17 @@ WSGI_APPLICATION = 'toolbox_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
 
 
 # Password validation
