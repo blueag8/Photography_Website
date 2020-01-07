d48f5b21f241b5d2f52ee0bea70d5a6a47a40803
blueag8 <41134758+blueag8@users.noreply.github.com>
2020-01-07 00:23:07 +0000

    Update slider.js

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,6 +1,8 @@
+// update the min and max values in the text box according to the selction on the range slider 
+
 function minValue(val) {
           document.getElementById('min_value').value=val; 
-  
+          
         }
 function maxValue(val) {
           document.getElementById('max_value').value=val; 
